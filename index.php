d354f019ca2b7c8e641747d0f34ef34d09dc2ec8
Udam Sathsara Liyanage <udam1998@gmail.com>
2017-07-08 10:06:10 +0000

    Added the line for Responsive Web Page

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <html lang="en-US">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <title>Learning Management System</title>
         <link rel="stylesheet" href="css/indexStyles.css">
         <link rel="stylesheet" href="css/commonStyles.css">
